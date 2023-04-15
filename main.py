da9f15cdaf474597de6ac6259851f55f22df148c
Luis Galeana <lsgaleana@gmail.com>
2023-04-15 15:23:43 -0700

    Initial files

---
+++ main.py
@@ -0,0 +1,11 @@
+import openai
+from prompt import initial_prompt
+
+def main():
+    print(initial_prompt)
+    # Prompt OpenAI
+    # Chat
+    # Do something
+
+if __name__ == "__main__":
+    main()
---
+++ prompt/__init__.py
@@ -0,0 +1,3 @@
+with open("prompt/initial_prompt.txt") as p:
+    initial_prompt = p.read()
+    p.close()
