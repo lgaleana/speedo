402c9ba1683ebbedd6ff29b3f5f10c93f16f27b1
Luis Galeana <lsgaleana@gmail.com>
2023-04-16 16:11:31 -0400

    NITs

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
     initial_prompt = f"{chat_prompt}\nToday is {today}\nSay hi."
     messages.append({"role": "system", "content": initial_prompt})
     assistant_message = chat_call(messages)
+    messages.append({"role": "assistant", "content": assistant_message})
     while True:
         print(f"\033[92m{assistant_message}")
         user_message = input("\033[1;34m")
